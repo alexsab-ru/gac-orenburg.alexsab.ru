8fa948ec3e2392ecd37a6e31b6cebf7733cbed6a
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-03-29 10:30:52 +0500

    use the desired tailwind config file

--- astro.local.config.mjs
+++ astro.local.config.mjs
@@ -10,7 +10,7 @@ import icon from "astro-icon";
 export default defineConfig({
 	integrations: [
 		tailwind({
-			configFile: './tailwind.gac.js'
+			configFile: './tailwind.gac-orenburg.js'
 		}),
 		sitemap(),
 		robots(),
